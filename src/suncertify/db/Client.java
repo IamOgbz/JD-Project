b8dcb42648e08ae2935d0e4a3e82d0560404fe12
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2014-04-24 08:16:21 +0530

    added main class

---
+++ src/suncertify/db/Client.java
@@ -0,0 +1,17 @@
+package suncertify.db;
+
+import java.util.logging.Logger;
+
+/**
+ * Command line client for testing operations
+ * @author Emmanuel
+ */
+public class Client {
+
+    private static final Logger log = Logger.getLogger("suncertify.db");
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
